da44085ff60df30436ab42da77807851cd4a58c5
Brooks Susor <susorbrooks@gmail.com>
2022-07-12 13:10:58 -0400

    Delete hover; select individual boxes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//create container
 const container = document.getElementById('container'); 
 
+//create grid
 for (let i = 0; i < 256; i++) { 
   const div = document.createElement('div'); 
   container.appendChild(div); 
   div.classList.add('etch'); 
 } 
+
+//select each individual grid
+const etch = document.querySelectorAll('.etch'); 
