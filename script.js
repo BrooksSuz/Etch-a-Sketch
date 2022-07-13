f3451ce0d9ea954a55eb9b953f745e9f6a00353b
Brooks Susor <susorbrooks@gmail.com>
2022-07-13 15:23:42 -0400

    Practice branch for square creation

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //create container
 const container = document.getElementById('container'); 
 
-//create grid
+//create grid 
 for (let i = 0; i < 256; i++) { 
   const div = document.createElement('div'); 
   container.appendChild(div); 
   div.classList.add('etch'); 
 } 
 
-//select each individual grid
+//select each individual box 
 const etch = document.querySelectorAll('.etch'); 
+
+container.addEventListener('mouseover', e => {
+  e.target.style.backgroundColor = 'white'; 
+}); 
