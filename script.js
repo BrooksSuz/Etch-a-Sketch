b7c3cdb742719055d998a35a003e5fa1bac51a54
Brooks Susor <susorbrooks@gmail.com>
2022-07-14 11:34:45 -0400

    Finish button functionality

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const container = document.getElementById('container'); 
 const divs = []; 
 const btn = document.querySelector('.btn'); 
+let userLength = 16; 
+let userWidth = 16; 
 
 //create grid 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < userLength; i++) {
   divs[i] = document.createElement('div'); 
   divs[i].classList.add('divs'); 
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < userWidth; j++) {
     const block = document.createElement('div'); 
     divs[i].appendChild(block); 
     block.classList.add('etch'); 
@@ -19,14 +21,41 @@ const etch = document.querySelectorAll('.etch');
 
 //event that allows user to create their own grid
 //max of 100 blocks
-btn.addEventListener('click', e => {
-  const userLength = prompt('Enter length (Maximum Length = 100)'); 
-  const userWidth = prompt('Enter Width (Maximum Width = 100'); 
-
+btn.addEventListener('click', () => {
   //delete previous grid
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < userLength; i++) {
     container.removeChild(document.querySelector('.divs')); 
   }
+
+  userLength = prompt('Enter length (Maximum Length = 100)'); 
+  userWidth = prompt('Enter Width (Maximum Width = 100'); 
+
+  if (userLength > 100 || userWidth > 100) {
+    //create grid 
+    for (let i = 0; i < userLength; i++) {
+      divs[i] = document.createElement('div'); 
+      divs[i].classList.add('divs'); 
+      for (let j = 0; j < userWidth; j++) {
+        const block = document.createElement('div'); 
+        divs[i].appendChild(block); 
+        block.classList.add('etch'); 
+      }
+      container.appendChild(divs[i]); 
+    } 
+    return alert('Value must be smaller than 100'); 
+  }
+
+  //create grid 
+  for (let i = 0; i < userLength; i++) {
+    divs[i] = document.createElement('div'); 
+    divs[i].classList.add('divs'); 
+    for (let j = 0; j < userWidth; j++) {
+      const block = document.createElement('div'); 
+      divs[i].appendChild(block); 
+      block.classList.add('etch'); 
+    }
+    container.appendChild(divs[i]); 
+  } 
 }); 
 
 //event that turns boxes from black to white 
