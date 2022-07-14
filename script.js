5ac831d0cceea35110d0f56e2364f8e6928fca90
Brooks Susor <susorbrooks@gmail.com>
2022-07-14 10:05:02 -0400

    Create functioning 16x16 Etch-a-Sketch

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-//create container
 const container = document.getElementById('container'); 
+const divs = []; 
 
 //create grid 
-for (let i = 0; i < 16; i++) { 
-  const block = document.createElement('div'); 
-  container.appendChild(block); 
-  block.classList.add('etch'); 
+for (let i = 0; i < 16; i++) {
+  divs[i] = document.createElement('div'); 
+  divs[i].classList.add('divs'); 
+  for (let j = 0; j < 16; j++) {
+    const block = document.createElement('div'); 
+    divs[i].appendChild(block); 
+    block.classList.add('etch'); 
+  }
+  container.appendChild(divs[i]); 
 } 
 
 //select each individual box 
