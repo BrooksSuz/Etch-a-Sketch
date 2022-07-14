4af25afa98166b606f01a294207ae2c08725f56c
Brooks Susor <susorbrooks@gmail.com>
2022-07-14 12:08:22 -0400

    Expand both button functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.getElementById('container'); 
 const divs = []; 
 const btn = document.querySelector('.btn'); 
+const btnReset = document.querySelector('.btn-reset'); 
 let userLength = 16; 
 let userWidth = 16; 
+let previousLength = userLength; 
+let previousWidth = userWidth; 
 
 //create grid 
 for (let i = 0; i < userLength; i++) {
@@ -30,19 +33,46 @@ btn.addEventListener('click', () => {
   userLength = prompt('Enter length (Maximum Length = 100)'); 
   userWidth = prompt('Enter Width (Maximum Width = 100'); 
 
-  if (userLength > 100 || userWidth > 100) {
-    //create grid 
-    for (let i = 0; i < userLength; i++) {
+  if (userLength > 100 || userWidth > 100) { 
+    //alert user
+    alert('Maximum value is 100');
+
+    //create grid
+    for (let i = 0; i < previousLength; i++) {
       divs[i] = document.createElement('div'); 
       divs[i].classList.add('divs'); 
-      for (let j = 0; j < userWidth; j++) {
+      for (let j = 0; j < previousWidth; j++) {
         const block = document.createElement('div'); 
         divs[i].appendChild(block); 
         block.classList.add('etch'); 
       }
       container.appendChild(divs[i]); 
     } 
-    return alert('Maximum value is 100'); 
+    return; 
+  }
+
+  //reassign previous length & width 
+  previousLength = userLength; 
+  previousWidth = userWidth; 
+
+
+  //create grid 
+  for (let i = 0; i < userLength; i++) {
+    divs[i] = document.createElement('div'); 
+    divs[i].classList.add('divs'); 
+    for (let j = 0; j < userWidth; j++) {
+      const block = document.createElement('div'); 
+      divs[i].appendChild(block); 
+      block.classList.add('etch'); 
+    }
+    container.appendChild(divs[i]); 
+  } 
+}); 
+
+btnReset.addEventListener('click', () => {
+  //delete previous grid
+  for (let i = 0; i < userLength; i++) {
+    container.removeChild(document.querySelector('.divs')); 
   }
 
   //create grid 
